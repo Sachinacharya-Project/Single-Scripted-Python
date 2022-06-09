3cf0574f291e3c51130f0b8f7355c8646d9c8d31
Sachin Acharya <51048248+sachin-acharya-projects@users.noreply.github.com>
2022-06-09 19:40:39 +0545

    Operator Overloading

---
+++ Operator Overloading.py/main.py
@@ -0,0 +1,15 @@
+# Reference: https://www.geeksforgeeks.org/operator-overloading-in-python/
+
+# Overloading + operator for classes
+
+class SpecialClass:
+    def __init__(self, params: str):
+        self.params = params
+    def __add__(self, obj: object):
+        "Overloading +"
+        return self.params + "separtor-in-between" + obj.params
+
+if __name__ == '__main__':
+    obj_1 = SpecialClass("Sachin")
+    obj_2 = SpecialClass("Acharya")
+    print(obj_1 + obj_2) # Adding Two Object - printing out
